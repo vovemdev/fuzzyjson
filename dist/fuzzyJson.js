50c78bdb010919e542830bd1d86a9ffaf50921d2
Ogunniyi Owamamwen.F <ogunniyiowamamwen@gmail.com>
2022-12-25 01:09:54 +0100

    fuzzyjson

--- dist/fuzzyJson.js
+++ dist/fuzzyJson.js
@@ -1,6 +1,6 @@
 /* 
 jsonKeyFlat v1.0.0 | open-source library for JavaScript and typeScript Module 
-Copyright 12/23/2022, 1:12:13 AM | VovemDev | MIT - license
+Copyright 12/23/2022, 1:12:13 AM | VovemDev | MIT-license
 */
 
 // flatten json
